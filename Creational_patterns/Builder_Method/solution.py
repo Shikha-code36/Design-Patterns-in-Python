bd06d4e4d39f6a7af5b85f2a0d4f7688559e3bec
Wisteli-ShikhaPandey <shikha.py36@gmail.com>
2022-08-05 19:44:16 +0530

    Builder Method

--- Creational_patterns/Builder_Method/solution.py
+++ Creational_patterns/Builder_Method/solution.py
@@ -2,71 +2,63 @@
 class Course:
  
     def __init__(self):
-        self.Fee()
+        self.Price()
         self.available_batches()
  
-    def Fee(self):
+    def Price(self):
         raise NotImplementedError
  
     def available_batches(self):
         raise NotImplementedError
  
     def __repr__(self):
-        return 'Fee : {0.fee} | Batches Available : {0.batches}'.format(self)
+        return 'Price : {0.price} | Batches Available : {0.batches}'.format(self)
 
 # concrete course
-class Cooking:
+class Cooking(Course):
  
     """ Class for Cooking """
  
-    def price(self):
-        return 11000
+    def Price(self):
+        self.price = 11000
     
     def available_batches(self):
         self.batches = 5
  
-    def __str__(self):
-        return "Cooking"
  
 # concrete course
-class Dancing:
+class Dancing(Course):
  
     """Class for Dancing"""
  
-    def price(self):
-        return 15000
+    def Price(self):
+        self.price = 15000
 
     def available_batches(self):
         self.batches = 4
- 
-    def __str__(self):
-        return "Dancing"
 
 # concrete course 
-class Coding:
+class Coding(Course):
  
     """Class for Coding"""
  
-    def price(self):
-        return 35000
+    def Price(self):
+        self.price = 35000
 
     def available_batches(self):
         self.batches = 9
- 
-    def __str__(self):
-        return "Coding"
  
 # Complex Course
 class ComplexCourse:
  
     def __repr__(self):
-        return 'Fee : {0.fee} | available_batches: {0.batches}'.format(self)
+        return 'Price : {0.price} | available_batches: {0.batches}'.format(self)
  
 # Complex course
 class Complexcourse(ComplexCourse):
  
-    def Fee(self):
-        self.fee = 7000
+    def Price(self):
+        self.price = 7000
  
     def available_batches(self):
         self.batches = 6
@@ -75,7 +67,7 @@ class Complexcourse(ComplexCourse):
 def construct_course(cls):
  
     course = cls()
-    course.Fee()
+    course.Price()
     course.available_batches()
  
     return course    # return the course object
@@ -88,4 +80,4 @@ if __name__ == "__main__":
     dance = Dancing()    # object for Dancing class
  
     complex_course = construct_course(Complexcourse)
-    print(complex_course) #Fee : 7000 | available_batches: 6
+    print(complex_course) #price : 7000 | available_batches: 6
